7bf57d6e70dc65799dacd64b1dd8b4dd94be3d4c
pallavi marbe <pallavimarbe10@gmail.com>
2024-01-31 23:52:44 +0530

    c

--- ai_image_generator/src/App.js
+++ ai_image_generator/src/App.js
@@ -4,9 +4,3 @@ import { ImageGenerator } from './Components/ImageGenerator/ImageGenerator';
 function App() {
   return (
     <div>
-      <ImageGenerator/>
-    </div>
-  );
-}
-
-export default App;
